d28b5e442824472b8473a13ed59ee7df80734032
jacob ickler <jaickler@comcast.net>
2018-08-21 20:55:23 -0400

    Made Grabber get player location and rotation. Also visualizes reach with debug line.

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -1,7 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Grabber.h"
 
+#include "Grabber.h"
+#include "Runtime/Engine/Public/DrawDebugHelpers.h"
+
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -30,5 +33,27 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint( 
+		OUT	PlayerViewPointLocation,
+		OUT	PlayerViewPointRotation
+	);
+	
+	/*UE_LOG(LogTemp, Warning, TEXT("PlayerViewPointLocation: %s"), *PlayerViewPointLocation.ToString());
+	UE_LOG(LogTemp, Warning, TEXT("PlayerViewPointRotation: %s"), *PlayerViewPointRotation.ToString());*/
+	float Reach = 100.f;
+	FVector LineTraceEnd = PlayerViewPointLocation + (PlayerViewPointRotation.Vector() * Reach);
+
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.f,
+		0.f,
+		10.f
+	);
 }
 
